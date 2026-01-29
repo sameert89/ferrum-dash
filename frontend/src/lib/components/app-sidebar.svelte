8f60847ad425ae9a0f80edcb804ee5035818a32f
Sameer Trivedi <sameer.trivedi@zeiss.com>
2026-01-29 18:29:54 +0530

    I am too tired to write a message

---
+++ frontend/src/lib/components/app-sidebar.svelte
@@ -0,0 +1,65 @@
+<script>
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import HomeIcon from "@lucide/svelte/icons/home";
+  import BarChart3Icon from "@lucide/svelte/icons/bar-chart-3"; import ZapIcon from "@lucide/svelte/icons/zap";
+  import TerminalIcon from "@lucide/svelte/icons/terminal";
+  import SettingsIcon from "@lucide/svelte/icons/settings";
+  
+  import logo from '$lib/assets/logo.png';
+
+  const items = [
+    {
+      title: "Home",
+      url: "/home",
+      icon: HomeIcon,
+    },
+    {
+      title: "Insights",
+      url: "/insights",
+      icon: BarChart3Icon,
+    },
+    {
+      title: "Webhooks",
+      url: "/webhooks",
+      icon: ZapIcon,
+    },
+    {
+      title: "Shell",
+      url: "/shell",
+      icon: TerminalIcon,
+    },
+    {
+      title: "Settings",
+      url: "/settings",
+      icon: SettingsIcon,
+    },
+  ];
+</script>
+
+<Sidebar.Root collapsible="icon">
+  <Sidebar.Header>
+    <img src={logo} alt ="App Logo" class="h-10 w-10 mx-auto mt-4 rounded-md"/>
+  </Sidebar.Header>
+    <Sidebar.Content class="flex items-center mx-auto">
+      <Sidebar.Group>
+        <Sidebar.GroupContent>
+          <Sidebar.Menu>
+            {#each items as item (item.title)}
+              <Sidebar.MenuItem>
+                <Sidebar.MenuButton size="xl" class="flex justify-center">
+                  {#snippet child({ props })}
+                    <a href={item.url} {...props}>
+                      <item.icon/>
+                      <span class="group-data-[collapsible=icon]:hidden">{item.title}</span>
+                    </a>
+                  {/snippet}
+                </Sidebar.MenuButton>
+              </Sidebar.MenuItem>
+            {/each}
+          </Sidebar.Menu>
+        </Sidebar.GroupContent>
+      </Sidebar.Group>
+    </Sidebar.Content>
+  <Sidebar.Footer class="flex items-center mt-auto">
+  </Sidebar.Footer>
+</Sidebar.Root>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import "../app.css";
+	import favicon from '$lib/assets/favicon.svg';
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import AppSidebar from "$lib/components/app-sidebar.svelte";
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Sidebar.Provider > 
+	<AppSidebar />
+	<main>
+		{@render children?.()}
+	</main>
+</Sidebar.Provider>
+
